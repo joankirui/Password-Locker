7176cc061f29c74dc4aafa135368ec3ae031ebcc
joankirui <joankirui99@gmail.com>
2021-10-23 14:31:04 +0300

    initial commit

---
+++ passlock.py
@@ -0,0 +1,39 @@
+class User:
+    '''
+    Class that generates new instances of user
+    '''
+
+    user_list = [] #Empty user list
+
+    def __init__(self,username,password):
+        '''
+        method that defines the properties of a user
+        '''
+        self.username = username
+        self.password = password
+    
+    def save_user(self):
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
+    @classmethod
+    def display_user(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user_list
+
+class Credentials:
+    '''
+    Create credentials class to help create new objects of credentials
+    '''
+    credentials_list = []   
+    def __init__(self,account,user_name,password):
+        '''
+        method that defines user credentials to be stored
+        '''
+        self.account = account
+        self.user_name = user_name
+        self.password = password
---
+++ passlock_test.py
@@ -0,0 +1,51 @@
+import unittest #Importing the unittest module
+from passlock import User #Importing the User class
+from passlock import Credentials #Importing the Credentials class
+
+class TestUser(unittest.TestCase):
+    '''
+    A Test class that defines test cases for the User class
+    '''
+    def setUp(self):
+        '''
+        Method that runs before each individual test methods run.
+        '''
+        self.new_user = User("JoanKirui","Wxyth1")
+    def test_init(self):
+        '''
+        test init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.username,"JoanKirui")
+        self.assertEqual(self.new_user.password,"Wxyth1")
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if a new user instance has been saved into the User list
+        '''
+        self.new_user.save_user() # saving the new contact
+        self.assertEqual(len(User.user_list),1)
+    def test_display_user(self):
+        '''
+        method that returns a list of all users saved
+        '''
+        self.assertEqual(User.display_user(),User.user_list)
+
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines test cases for the Credentials class behaviours
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credentials = Credentials("Ig","Joan_Kirui","weg5uytg")
+    def test_init(self):
+        '''
+        Test case to check if a new Credentials instance has been initialized correctly
+        '''
+        self.assertEqual(self.new_credentials.account,"Ig")
+        self.assertEqual(self.new_credentials.user_name,"Joan_Kirui")
+        self.assertEqual(self.new_credentials.password,"weg5uytg")
+        
+if __name__ == "__main__":
+    unittest.main()
