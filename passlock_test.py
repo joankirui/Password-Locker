66ebb8d101ea5ee530f359f5c3feef0ec6a1c58b
joankirui <joankirui99@gmail.com>
2021-10-23 15:27:45 +0300

    create a test case for deleting multiple credentials

--- passlock_test.py
+++ passlock_test.py
@@ -67,6 +67,17 @@ class TestCredentials(unittest.TestCase):
         test_credentials = Credentials("Gmail","Lalakirui","we56fng")  #new Credentials
         test_credentials.save_credentials()
         self.assertEqual(len(Credentials.credentials_list),2)
+
+    def test_delete_credentials(self):
+        '''
+        test_delete_credentials to test if we can remove a Credential from our credentials list
+        '''
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("Gmail","Lalakirui","we56fng") #new credentials
+        test_credentials.save_credentials()
+
+        self.new_credentials.delete_credentials() #deleting a credentials object
+        self.assertEqual(len(Credentials.credentials_list),1)
         
 if __name__ == "__main__":
     unittest.main()
